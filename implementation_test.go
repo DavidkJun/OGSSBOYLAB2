d909adac75ddb2944eff7d062c355be4273a0c78
Lev <lev.bereza@gmail.com>
2025-03-19 18:19:18 +0200

    feat: add tests

--- implementation_test.go
+++ implementation_test.go
@@ -7,17 +7,80 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+func TestPrefixToInfix(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+		err      error
+	}{
+		{
+			name:     "simple addition",
+			input:    "+ 2 3",
+			expected: "(2 + 3)",
+			err:      nil,
+		},
+		{
+			name:     "complex expression",
+			input:    "* + 2 3 - 4 1",
+			expected: "((2 + 3) * (4 - 1))",
+			err:      nil,
+		},
+		{
+			name:     "invalid expression",
+			input:    "+ 2",
+			expected: "",
+			err:      fmt.Errorf("invalid expression"),
+		},
+		{
+			name:     "invalid token",
+			input:    "+ 2 a",
+			expected: "",
+			err:      fmt.Errorf("invalid token: a"),
+		},
+		{
+			name:     "empty input",
+			input:    "",
+			expected: "",
+			err:      fmt.Errorf("empty input"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := PrefixToInfix(tt.input)
+			if tt.err != nil {
+				fmt.Println(tt.err)
+				assert.EqualError(t, err, tt.err.Error())
+			} else {
+				assert.Nil(t, err)
+				assert.Equal(t, tt.expected, res)
+			}
+		})
 	}
 }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
+func ExamplePrefixToInfix() {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"+ 2 3", "(2 + 3)"},
+		{"* + 2 3 - 4 1", "((2 + 3) * (4 - 1))"},
+		{"+ 2", ""},
+		{"+ 2 a", ""},
+		{"", ""},
+	}
+
+	for _, tt := range tests {
+		res, _ := PrefixToInfix(tt.input)
+		fmt.Println(res)
+	}
 
 	// Output:
-	// 2 2 +
+	// (2 + 3)
+	// ((2 + 3) * (4 - 1))
+	//
+	//
+	//
 }
